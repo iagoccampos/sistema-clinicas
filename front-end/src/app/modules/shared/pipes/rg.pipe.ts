b56f9c1f146de0be2b468e4052e5c2b91fb65989
Iago Coutinho Campos <iago.campos@leucotron.com.br>
2021-10-31 21:34:54 -0300

    Correção para string vazia

--- front-end/src/app/modules/shared/pipes/rg.pipe.ts
+++ front-end/src/app/modules/shared/pipes/rg.pipe.ts
@@ -7,6 +7,6 @@ export class RgPipe implements PipeTransform {
 
 	transform(value: string, ...args: unknown[]) {
 		// 3005470 > 3.005.470
-		return [value.substr(0, 1), value.substr(1, 3), value.substr(4, 3)].join('.')
+		return value ? [value.substr(0, 1), value.substr(1, 3), value.substr(4, 3)].join('.') : ''
 	}
 }
