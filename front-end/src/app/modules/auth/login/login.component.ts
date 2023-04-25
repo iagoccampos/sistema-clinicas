7ce3c989cd0c95a2a0a029356829dc5f05ebc4a9
Iago C. Campos <iagoccampos@hotmail.com>
2023-04-25 20:03:20 -0300

    Better performance from LightHouse

--- front-end/src/app/modules/auth/login/login.component.ts
+++ front-end/src/app/modules/auth/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { AuthService } from '../../../services/auth.service'
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss']
+	styleUrls: ['./login.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
 	hide = true
